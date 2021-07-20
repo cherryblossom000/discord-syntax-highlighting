bedf6a20a557f9d9c51fbb2f0aa1cb9ca7253e2c
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2021-07-20 15:26:43 +1000

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+hljs.debugMode()
+hljs.highlightAll()
+
+document.querySelectorAll('.categories > li').forEach(category => {
+  category.addEventListener('click', event => {
+    const current = document.querySelector('.categories .current')
+    const currentCategory = current.dataset.category
+    const nextCategory = event.target.dataset.category
+
+    if (currentCategory !== nextCategory) {
+      current.classList.remove('current')
+      event.target.classList.add('current')
+
+      document
+        .querySelectorAll(`.${currentCategory}`)
+        .forEach(language => language.classList.add('hidden'))
+      document
+        .querySelectorAll(`.${nextCategory}`)
+        .forEach(language => language.classList.remove('hidden'))
+
+      window.scrollTo(0, 0)
+    }
+  })
+})
+
+document.getElementById('light-theme').addEventListener('input', () => {
+  document.documentElement.classList.toggle('theme-dark')
+  document.documentElement.classList.toggle('theme-light')
+})
